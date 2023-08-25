8dd142f02ce4ecb19b39d47aa41c3aca2604dc0a
Pedro <pedrofuentespi@gmail.com>
2023-08-25 09:03:34 +0200

    Add about navigotion in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,6 +7,9 @@ export default function Home() {
       <h1 className="text-5xl font-bold">
         Next 13+
       </h1>
+      <Link href="/about">
+        <span className='text-2xl'>About Page</span>
+      </Link>
     </main>
   )
 }
